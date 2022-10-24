0c3cf65ba9342239cbb2a45058b084f9e4cc290b
Yannick McCabe-Costa <yannick@mccabecosta.com>
2022-10-24 18:55:42 +0100

    Deliver bootstrap.css from CSNJS

--- html/includes/head.php
+++ html/includes/head.php
@@ -2,8 +2,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <meta name="author" content="Yannick McCabe-Costa">
 <!-- Bootstrap core CSS -->
-<link rel="stylesheet" href="<?php echo APP_HTTP_SCHEME; ?><?php echo APP_URL_SLASH; ?>assets/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-<?php // <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/cookieconsent@3/build/cookieconsent.min.css" />?>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/4.6.2/css/bootstrap.min.css" integrity="sha512-rt/SrQ4UNIaGfDyEXZtNcyWvQeOq0QLygHluFQcSjaGB04IxWhal71tKuzP6K8eYXYB6vJV4pHkXcmFGGQ1/0w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 <!-- Custom styles for this template -->
 <link rel="stylesheet" href="<?php echo APP_HTTP_SCHEME; ?><?php echo APP_URL_SLASH; ?>assets/css/yan.custom.2021041301.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
