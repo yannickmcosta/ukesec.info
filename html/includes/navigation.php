f17167177026f522013476b49ffc6d0b1d069f81
Yannick McCabe-Costa <yannick@mccabecosta.com>
2022-10-25 22:52:38 +0100

    Link to Calculator with Schedule

--- html/includes/navigation.php
+++ html/includes/navigation.php
@@ -28,11 +28,11 @@
 				<!-- Nav Item Separator -->
 				<?php if ($_SERVER['REQUEST_URI'] == "/calculator") { ?>
 					<li class="nav-item active">
-						<a class="nav-link" href="<?php echo APP_HTTP_SCHEME; ?><?php echo APP_URL_SLASH; ?>calculator">Calculator <span class="sr-only">(current)</span></a>
+						<a class="nav-link" href="<?php echo APP_HTTP_SCHEME; ?><?php echo APP_URL_SLASH; ?>calculator-schedule">Calculator <span class="sr-only">(current)</span></a>
 					</li>
 				<?php } else { ?>
 					<li class="nav-item">
-						<a class="nav-link" href="<?php echo APP_HTTP_SCHEME; ?><?php echo APP_URL_SLASH; ?>calculator">Calculator</a>
+						<a class="nav-link" href="<?php echo APP_HTTP_SCHEME; ?><?php echo APP_URL_SLASH; ?>calculator-schedule">Calculator</a>
 					</li>
 				<?php } ?>
 			</ul>
