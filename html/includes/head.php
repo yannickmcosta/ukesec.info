68710cc487392782bb8e2b15370f5d44271ab147
Yannick McCabe-Costa <yannickmcosta@users.noreply.github.com>
2022-11-05 10:01:17 +0000

    Update head.php
    Disablement of plausible

--- html/includes/head.php
+++ html/includes/head.php
@@ -6,4 +6,3 @@
 <!-- Custom styles for this template -->
 <link rel="stylesheet" href="<?php echo APP_HTTP_SCHEME; ?><?php echo APP_URL_SLASH; ?>assets/css/yan.custom.2022102502.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-<script defer data-domain="ukesec.info" src="https://plausible.ukesec.info/js/plausible.js"></script>
